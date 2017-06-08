fabf8fa764b5c2f68e9237513a19f6699c05f2c2
noe33000 <champignyn@gmail.com>
2017-06-08 10:09:20 +0200

    test

--- app/components/home/home.js
+++ app/components/home/home.js
@@ -7,39 +7,74 @@ angular.module("todo")
         controller: Main
     })
 
-;
+    ;
 
 function alertMe(name) {
     alert(name);
 }
 
 function deletePerson(persons, index) {
-    persons.splice(1, index);
+    alert(index);
+    persons.splice(index, 1);
 }
 
 function Main() {
     this.name = "";
-    this.title = "cest la home page!";
+    this.title = "C'est la home page!";
     this.alertMyName = alertMe;
     this.deletePerson = deletePerson;
 
     this.persons = [
         {
+            id: 0,
             name: "Jonathan",
-            age: 25
+            lastName: "Collinet",
+            age: 25,
         },
         {
+            id: 1,
             name: "Julien",
-            age: 25
+            lastName: "Swag",
+            age: 25,
         },
         {
+            id: 2,
             name: "Johan",
+            lastName: "tello",
+            age: 25,
+        },
+        {
+            id: 3,
+            name: "Johnny",
+            lastName: "Holliday",
             age: 25
         },
         {
-            name: "Johnny",
-            age: 25
+            id: 4,
+            name: "michel",
+            lastName: "Trello",
+            age: 1000
+        },
+        {
+            id: 5,
+            name: "michael",
+            lastName: "jackson",
+            age: 8
+        },
+        {
+            id: 6,
+            name: "André",
+            lastName: "Goubil",
+            age: 87
         }
     ]
+}
+var params = {
+    type: 'artist',
+    id: '3000'
+}
+
+function request(type, params) {
+    url = "http://discogs.com/" + params.type + "/" + params.id; 
 }
 
